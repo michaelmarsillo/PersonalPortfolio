2d7737c56d266da7b160c8e31b929c57c72a687a
michaelmarsillo <thecaptain49@hotmail.ca>
2025-12-02 01:10:04 -0500

    added audiofy to projects, and made minor project description tweaks

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -5,7 +5,7 @@ const Projects = () => {
     const projects = [
         {
             title: "CVLens",
-            description: "Fullstack app that extracts data from resumes in PDF/DOCX formats. Built NLP model using spaCy to pull details like education, skills, and work experience. Super clean and minimalist frontend made with Next.js and Tailwind.",
+            description: "Fullstack app that extracts data from resumes in PDF/DOCX formats. Built NLP model using spaCy to pull details like education, skills, and work experience. Super clean and minimalist frontend made with Next.js and Tailwind. 🔍",
             imageUrl: "/images/cvlens.png",
             techStack: ["Python", "Next.js", "Tailwind", "spaCy", "FastAPI", "Docker", "Git",],
             githubUrl: "https://github.com/CVLens/CVLens",
@@ -13,15 +13,23 @@ const Projects = () => {
         },
         {
             title: "Lucky Lease",
-            description: "A web-based platform for short-term subletting, focused on student and intern housing, like Facebook Marketplace, but purpose-built for rentals.🍀",
+            description: "A web-based platform for short-term subletting, focused on student and intern housing, like Facebook Marketplace, but purpose-built for rentals. 🍀",
             imageUrl: "/images/luckylease1.png",
             techStack: ["TypeScript", "Next.js", "Tailwind", "Express.js", "Supabase", "Socket.io", "Gemini API", "Git",],
             githubUrl: "https://github.com/TaseskiCS/LuckyLease",
             demoUrl: "https://luckylease.co/"
         },
         {
+            title: "Audiofy",
+            description: "Music trivia game. Guess artists from 7-second song previews. Built with multiple game modes (Solo, Multiplayer, Heardle, Arcade), and a global leaderboard. Inspired by the family game I used to play at the cottage. 🎵",
+            imageUrl: "/images/audiofyhome.png",
+            techStack: ["TypeScript", "Next.js", "Tailwind", "Node.js", "Express.js", "Socket.io", "PostgreSQL", "Git",],
+            githubUrl: "https://github.com/michaelmarsillo/Audiofy",
+            demoUrl: "https://audiofy-m.vercel.app/"
+        },
+        {
             title: "Roomify",
-            description: "MERN-stack web application that allows Canadian users to create an account, calculate their total TFSA contribution room, and manage their deposits and withdrawals. Shipped in 24hrs 🚀",
+            description: "MERN-stack web application that allows Canadian users to create an account, calculate their total TFSA contribution room, and manage their deposits and withdrawals. Shipped in 24hrs. 🚀",
             imageUrl: "/images/Roomify.png",
             techStack: ["JavaScript", "Next.js", "Tailwind", "Express.js", "Node.js", "MongoDB", "Postman", "jsonwebtoken", "bcryptjs", "Git",],
             githubUrl: "https://github.com/michaelmarsillo/Roomify",
@@ -29,7 +37,7 @@ const Projects = () => {
         },
         {
             title: "MarxPDF",
-            description: "A website designed to automate the process of filling out the ULT, PG.1 and JHA PDF forms for 40+ employees at the utility locating company I previously worked at.",
+            description: "A web app designed to automate the process of filling out the ULT, PG.1 and JHA PDF forms for 40+ employees at the utility locating company I previously worked at. 📄",
             imageUrl: "/images/marxpdf.png",
             techStack: ["React.js", "JavaScript", "HTML", "CSS", "pdf-lib", "Git",],
             githubUrl: "https://github.com/michaelmarsillo/RPAforPDF",
@@ -37,7 +45,7 @@ const Projects = () => {
         },
         {
             title: "Portfolio Website",
-            description: "My personal wesbite showcasing my work, skills, and a little bit more about me. A good place to highlight my web dev abilities and what i've built.",
+            description: "My personal wesbite showcasing my work, skills, and a little bit more about me. A good place to highlight my web dev abilities and what i've built. 🌐",
             imageUrl: "/images/portfoliowebsite.png",
             techStack: ["React.js", "Tailwind", "JavaScript", "HTML", "Git",],
             githubUrl: "https://github.com/michaelmarsillo/PersonalPortfolio",
